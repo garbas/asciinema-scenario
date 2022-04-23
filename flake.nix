653c85f4cb4190530bbf5599875ad686528d82f1
Rok Garbas <rok@garbas.si>
2022-04-23 23:04:31 +0200

    Improve nix and ci setup (#11)
    * Few setup improvements
    
    * add direnv integration for those who use it
    * get assigned to each PR (via CODEOWNERS file)
    * run `nix flake check` in CI
    * use rust-overlay
    * format everything with the help of pre-commit-hooks
    
    * adding shell.nix and defaul.nix for compatibility reasons
    
    * fix CODEOWNERS file
    
    * bump cachix-action github action

--- flake.nix
+++ flake.nix
@@ -1,41 +1,102 @@
 {
   description = "asciinema-scenario";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    naersk.url = "github:nmattia/naersk";
-  };
+  nixConfig.extra-substituters = "https://asciinema-scenario.cachix.org";
+  nixConfig.extra-trusted-public-keys = "asciinema-scenario.cachix.org-1:ql4s3RvH3xgL+TmgOkAvjZEZV8ucc4Qx/bKBQ483BUE=";
 
-  outputs =
-    { self
-    , nixpkgs
-    , naersk
-    }:
-    let
-      systems =
-        [ "x86_64-linux"
-          "i686-linux"
-          "x86_64-darwin"
-          "aarch64-linux"
-        ];
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore.url = "github:hercules-ci/gitignore.nix";
+  inputs.gitignore.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.naersk.url = "github:nix-community/naersk";
+  inputs.naersk.inputs.nixpkgs.follows = "nixpkgs";
 
-      mkProject = system:
-        let
-          pkgs = nixpkgs.legacyPackages."${system}";
-          inherit (pkgs) stdenv;
-          inherit (naersk.lib."${system}") buildPackage;
-        in buildPackage {
-          root = ./.;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-compat,
+    flake-utils,
+    gitignore,
+    pre-commit-hooks,
+    rust-overlay,
+    naersk,
+  }:
+    flake-utils.lib.eachSystem
+    [
+      flake-utils.lib.system.x86_64-linux
+      flake-utils.lib.system.aarch64-linux
+      flake-utils.lib.system.x86_64-darwin
+    ]
+    (
+      system: let
+        inherit (nixpkgs) lib;
+
+        readTOML = file: builtins.fromTOML (builtins.readFile file);
+        cargoTOML = readTOML ./Cargo.toml;
+        warnToUpdateNix = lib.warn "Consider updating to Nix > 2.7 to remove this warning!";
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+
+        rust = let
+          _rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analysis"
+              "rls-preview"
+              "rustfmt-preview"
+              "clippy-preview"
+            ];
+          };
+        in
+          pkgs.buildEnv {
+            name = _rust.name;
+            inherit (_rust) meta;
+            buildInputs = [pkgs.makeWrapper];
+            paths = [_rust];
+            pathsToLink = ["/" "/bin"];
+            # XXX: This is needed because cargo and clippy commands need to
+            # also be aware of other binaries in order to work properly.
+            # https://github.com/cachix/pre-commit-hooks.nix/issues/126
+            postBuild = ''
+              for i in $out/bin/*; do
+                wrapProgram "$i" --prefix PATH : "$out/bin"
+              done
+            '';
+          };
+
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            alejandra = {
+              enable = true;
+            };
+            rustfmt = {
+              enable = true;
+              entry = lib.mkForce "${rust}/bin/cargo-fmt fmt -- --check --color always";
+            };
+          };
+        };
+
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+
+        asciinema-scenario = naersk-lib.buildPackage {
+          inherit (cargoTOML.package) name version;
+          root = gitignore.lib.gitignoreSource self;
           buildInputs = with pkgs; [
-            rustPackages.rustc
-            rustPackages.clippy
-            rustPackages.rls
-            rustPackages.rustfmt
-
-            cargo-graph
-            cargo-edit
-            cargo-release
-
             python3
             fd
             entr
@@ -44,23 +105,19 @@
             export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
           '';
         };
+      in rec {
+        packages = {inherit asciinema-scenario;};
+        packages.default = packages.asciinema-scenario;
 
-      addProject = attr_name: packages:
-        let
-          inherit (nixpkgs.lib) recursiveUpdate genAttrs;
-          packageForAllSystems = genAttrs systems (system: { "${attr_name}"= mkProject system; });
-        in
-          recursiveUpdate packages packageForAllSystems;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [rust];
+          shellHook = pre-commit.shellHook;
+        };
 
+        checks = {inherit pre-commit asciinema-scenario;};
 
-      defaultProject = attr_name: packages:
-        let
-          inherit (nixpkgs.lib) genAttrs;
-        in
-          genAttrs systems (system: packages."${system}"."${attr_name}");
-    in
-      rec {
-        packages = addProject "asciinema_scenario" {};
-        defaultPackage = defaultProject "asciinema_scenario" packages;
-      };
+        defaultPackage = warnToUpdateNix packages.default;
+        devShell = warnToUpdateNix devShells.default;
+      }
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -32,262 +32,263 @@ const SVG_FOOTER: &'static str = "</text>
 
 #[derive(Deserialize, Debug)]
 struct ScenarioHeader {
-  #[serde(default = "default_step")]
-  step: f64,
+    #[serde(default = "default_step")]
+    step: f64,
 
-  #[serde(default = "default_width")]
-  width: u32,
+    #[serde(default = "default_width")]
+    width: u32,
 
-  #[serde(default = "default_height")]
-  height: u32,
+    #[serde(default = "default_height")]
+    height: u32,
 }
 
 fn default_step() -> f64 {
-  0.10
+    0.10
 }
 
 fn default_width() -> u32 {
-  77
+    77
 }
 
 fn default_height() -> u32 {
-  20
+    20
 }
 
 fn print_entry(entry: Entry) -> Result<(), Error> {
-  println!("{}", to_string(&entry)?);
-  Ok(())
+    println!("{}", to_string(&entry)?);
+    Ok(())
 }
 
 fn clear_terminal(time: &mut f64, step: &f64) -> Result<(), Error> {
-  *time += 18.0 * step;
-  print_entry(Entry {
-    time: *time,
-    event_type: EventType::Output,
-    event_data: "\r\x1b[2J\r\x1b[H".to_string(),
-  })?;
-  *time += 3.0 * step;
-  Ok(())
+    *time += 18.0 * step;
+    print_entry(Entry {
+        time: *time,
+        event_type: EventType::Output,
+        event_data: "\r\x1b[2J\r\x1b[H".to_string(),
+    })?;
+    *time += 3.0 * step;
+    Ok(())
 }
 
 fn echo_typing(time: &mut f64, step: &f64, line_raw: &str) -> Result<String, Error> {
-  let mut bright_applied = false;
-  for char in line_raw.to_string().chars() {
-    *time += step;
-    if char == '#' {
-      print_entry(Entry {
+    let mut bright_applied = false;
+    for char in line_raw.to_string().chars() {
+        *time += step;
+        if char == '#' {
+            print_entry(Entry {
+                time: *time,
+                event_type: EventType::Output,
+                event_data: "\x1b[1m".to_string(),
+            })?;
+            bright_applied = true;
+        }
+        print_entry(Entry {
+            time: *time,
+            event_type: EventType::Output,
+            event_data: char.to_string(),
+        })?;
+    }
+    // clear
+    if bright_applied {
+        print_entry(Entry {
+            time: *time,
+            event_type: EventType::Output,
+            event_data: "\x1b[0m".to_string(),
+        })?;
+    }
+
+    *time += 3.0 * step;
+    print_entry(Entry {
         time: *time,
         event_type: EventType::Output,
-        event_data: "\x1b[1m".to_string(),
-      })?;
-      bright_applied = true;
-    }
-    print_entry(Entry {
-      time: *time,
-      event_type: EventType::Output,
-      event_data: char.to_string(),
+        event_data: "\r\n".to_string(),
     })?;
-  }
-  // clear
-  if bright_applied {
-    print_entry(Entry {
-      time: *time,
-      event_type: EventType::Output,
-      event_data: "\x1b[0m".to_string(),
-    })?;
-  }
 
-  *time += 3.0 * step;
-  print_entry(Entry {
-    time: *time,
-    event_type: EventType::Output,
-    event_data: "\r\n".to_string(),
-  })?;
-
-  let parts: Vec<&str> = line_raw.splitn(2, "#").collect();
-  Ok(if parts.len() == 1 {
-    parts[0].to_string()
-  } else {
-    format!(
-      "{}<tspan class=\"fg-15\">#{}</tspan>",
-      parts[0].to_string(),
-      parts.clone().split_off(1).join(""),
-    )
-  })
+    let parts: Vec<&str> = line_raw.splitn(2, "#").collect();
+    Ok(if parts.len() == 1 {
+        parts[0].to_string()
+    } else {
+        format!(
+            "{}<tspan class=\"fg-15\">#{}</tspan>",
+            parts[0].to_string(),
+            parts.clone().split_off(1).join(""),
+        )
+    })
 }
 
 fn echo_console_line(
-  time: &mut f64,
-  step: &f64,
-  prompt: &str,
-  line: &str,
+    time: &mut f64,
+    step: &f64,
+    prompt: &str,
+    line: &str,
 ) -> Result<Vec<String>, Error> {
-  *time += step;
+    *time += step;
 
-  let prompt_line: String;
-  let mut preview_lines: Vec<String> = vec![];
+    let prompt_line: String;
+    let mut preview_lines: Vec<String> = vec![];
 
-  if prompt != "" {
-    prompt_line = format!("\x1b[32m{}\x1b[0m$ ", prompt);
-    preview_lines.push(format!("<tspan class=\"fg-2\">{}</tspan>$ ", prompt));
-  } else {
-    prompt_line = "$ ".to_string();
-    preview_lines.push("$ ".to_string());
-  };
+    if prompt != "" {
+        prompt_line = format!("\x1b[32m{}\x1b[0m$ ", prompt);
+        preview_lines.push(format!("<tspan class=\"fg-2\">{}</tspan>$ ", prompt));
+    } else {
+        prompt_line = "$ ".to_string();
+        preview_lines.push("$ ".to_string());
+    };
 
-  print_entry(Entry {
-    time: *time,
-    event_type: EventType::Output,
-    event_data: prompt_line,
-  })?;
+    print_entry(Entry {
+        time: *time,
+        event_type: EventType::Output,
+        event_data: prompt_line,
+    })?;
 
-  *time += 3.0 * step;
+    *time += 3.0 * step;
 
-  preview_lines.push(echo_typing(time, step, line)?);
+    preview_lines.push(echo_typing(time, step, line)?);
 
-  Ok(preview_lines)
+    Ok(preview_lines)
 }
 
 #[derive(Debug, StructOpt)]
 #[structopt(
-  name = "asciinema-scenario",
-  about = "Create asciinema videos from a text file."
+    name = "asciinema-scenario",
+    about = "Create asciinema videos from a text file."
 )]
 struct Cli {
-  #[structopt(flatten)]
-  verbose: Verbose,
+    #[structopt(flatten)]
+    verbose: Verbose,
 
-  scenario_file: String,
+    scenario_file: String,
 
-  #[structopt(name = "preview-file", long, short)]
-  svg_preview_file: Option<String>,
+    #[structopt(name = "preview-file", long, short)]
+    svg_preview_file: Option<String>,
 }
 
 fn main() -> Result<(), Error> {
-  let cli = Cli::from_args();
+    let cli = Cli::from_args();
 
-  // Initialize logging
-  let log_level = cli.verbose.get_level_filter();
+    // Initialize logging
+    let log_level = cli.verbose.get_level_filter();
 
-  // stdout/stderr based logger
-  TermLogger::init(
-    log_level,            // set log level via "-vvv" flags
-    Config::default(),    // how to format logs
-    TerminalMode::Stderr, // log to stderr
-  )?;
+    // stdout/stderr based logger
+    TermLogger::init(
+        log_level,            // set log level via "-vvv" flags
+        Config::default(),    // how to format logs
+        TerminalMode::Stderr, // log to stderr
+    )?;
 
-  // check if does not scenario_file exists
-  if !Path::new(&cli.scenario_file).exists() {
-    println!(
-      "\x1b[31mERROR:\x1b[0m scenario file `{}` does not exist!",
-      cli.scenario_file
-    );
-    exit(1);
-  }
+    // check if does not scenario_file exists
+    if !Path::new(&cli.scenario_file).exists() {
+        println!(
+            "\x1b[31mERROR:\x1b[0m scenario file `{}` does not exist!",
+            cli.scenario_file
+        );
+        exit(1);
+    }
 
-  // check if svg_preview_file exists
-  if cli.svg_preview_file.is_some() && Path::new(cli.svg_preview_file.as_ref().unwrap()).exists() {
-    println!(
-      "\x1b[31mERROR:\x1b[0m svg preview file `{}` already exist!",
-      cli.svg_preview_file.unwrap()
-    );
-    exit(1);
-  }
+    // check if svg_preview_file exists
+    if cli.svg_preview_file.is_some() && Path::new(cli.svg_preview_file.as_ref().unwrap()).exists()
+    {
+        println!(
+            "\x1b[31mERROR:\x1b[0m svg preview file `{}` already exist!",
+            cli.svg_preview_file.unwrap()
+        );
+        exit(1);
+    }
 
-  // Read lines from scenario_file
-  let first_f = File::open(&cli.scenario_file)?;
-  let mut first_reader = BufReader::new(first_f);
+    // Read lines from scenario_file
+    let first_f = File::open(&cli.scenario_file)?;
+    let mut first_reader = BufReader::new(first_f);
 
-  // Header
-  let mut first_line = String::new();
-  first_reader.read_line(&mut first_line)?;
+    // Header
+    let mut first_line = String::new();
+    first_reader.read_line(&mut first_line)?;
 
-  let header: ScenarioHeader = if first_line.starts_with("#! ") {
-    from_str(&first_line[3..])?
-  } else {
-    from_str(&"{}")?
-  };
-  let asciicast_header = Header {
-    version: 2,
-    width: header.width,
-    height: header.height,
-    timestamp: None,
-    duration: None,
-    idle_time_limit: None,
-    command: None,
-    title: None,
-    env: None,
-  };
-  println!("{}", to_string(&asciicast_header)?);
-
-  // The rest of the file
-  // Read lines from scenario_file
-  let mut preview_lines: Vec<Vec<String>> = vec![];
-  let f = File::open(cli.scenario_file)?;
-  let reader = BufReader::new(f);
-  let mut time = 3.0 * header.step;
-  for (index, maybe_line) in reader.lines().enumerate() {
-    let line = maybe_line?;
-    // skip when first line starts with "#! " since we already processed it above
-    if index == 0 && line.starts_with("#! ") {
-      continue;
-
-    // skip lines starting with "#"
-    } else if line.starts_with("#") {
-      continue;
-
-    // lines starting with "$ " display as console lines
-    } else if line.starts_with("$ ") {
-      preview_lines.push(echo_console_line(&mut time, &header.step, "", &line[2..])?);
-
-    // lines starting with "(nix-shell) $ " display as console lines
-    } else if line.starts_with("(nix-shell) $ ") {
-      preview_lines.push(echo_console_line(
-        &mut time,
-        &header.step,
-        "(nix-shell) ",
-        &line[14..],
-      )?);
-
-    // lines starting with "--" will clear display
-    } else if line.starts_with("--") {
-      clear_terminal(&mut time, &header.step)?;
-
-    // timeout
-    } else if line.trim() == "" {
-      time += 3.0 * header.step;
-
-    // everything else print immediately
+    let header: ScenarioHeader = if first_line.starts_with("#! ") {
+        from_str(&first_line[3..])?
     } else {
-      print_entry(Entry {
-        time: time,
-        event_type: EventType::Output,
-        event_data: format!("{}\r\n", line.clone()),
-      })?;
-      preview_lines.push(vec![line.to_string()]);
-    }
-  }
+        from_str(&"{}")?
+    };
+    let asciicast_header = Header {
+        version: 2,
+        width: header.width,
+        height: header.height,
+        timestamp: None,
+        duration: None,
+        idle_time_limit: None,
+        command: None,
+        title: None,
+        env: None,
+    };
+    println!("{}", to_string(&asciicast_header)?);
 
-  match cli.svg_preview_file {
-    Some(filename) => {
-      write(
-        filename,
-        format!(
-          "{}{}{}",
-          SVG_HEADER,
-          preview_lines
-            .into_iter()
-            .map(|line_items| format!(
-              "<tspan x=\"0\" dy=\"1.2em\">{}</tspan>",
-              line_items.join("")
-            ))
-            .collect::<Vec<String>>()
-            .join(""),
-          SVG_FOOTER,
-        ),
-      )?;
-      Ok(())
+    // The rest of the file
+    // Read lines from scenario_file
+    let mut preview_lines: Vec<Vec<String>> = vec![];
+    let f = File::open(cli.scenario_file)?;
+    let reader = BufReader::new(f);
+    let mut time = 3.0 * header.step;
+    for (index, maybe_line) in reader.lines().enumerate() {
+        let line = maybe_line?;
+        // skip when first line starts with "#! " since we already processed it above
+        if index == 0 && line.starts_with("#! ") {
+            continue;
+
+        // skip lines starting with "#"
+        } else if line.starts_with("#") {
+            continue;
+
+        // lines starting with "$ " display as console lines
+        } else if line.starts_with("$ ") {
+            preview_lines.push(echo_console_line(&mut time, &header.step, "", &line[2..])?);
+
+        // lines starting with "(nix-shell) $ " display as console lines
+        } else if line.starts_with("(nix-shell) $ ") {
+            preview_lines.push(echo_console_line(
+                &mut time,
+                &header.step,
+                "(nix-shell) ",
+                &line[14..],
+            )?);
+
+        // lines starting with "--" will clear display
+        } else if line.starts_with("--") {
+            clear_terminal(&mut time, &header.step)?;
+
+        // timeout
+        } else if line.trim() == "" {
+            time += 3.0 * header.step;
+
+        // everything else print immediately
+        } else {
+            print_entry(Entry {
+                time: time,
+                event_type: EventType::Output,
+                event_data: format!("{}\r\n", line.clone()),
+            })?;
+            preview_lines.push(vec![line.to_string()]);
+        }
     }
-    None => Ok(()),
-  }
+
+    match cli.svg_preview_file {
+        Some(filename) => {
+            write(
+                filename,
+                format!(
+                    "{}{}{}",
+                    SVG_HEADER,
+                    preview_lines
+                        .into_iter()
+                        .map(|line_items| format!(
+                            "<tspan x=\"0\" dy=\"1.2em\">{}</tspan>",
+                            line_items.join("")
+                        ))
+                        .collect::<Vec<String>>()
+                        .join(""),
+                    SVG_FOOTER,
+                ),
+            )?;
+            Ok(())
+        }
+        None => Ok(()),
+    }
 }
